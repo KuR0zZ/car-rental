7f39f9f9f6334e4e49dbfdf6b0a1b2d8c1b5262c
KuR0zZ <ferdinandiphone6@gmail.com>
2024-12-18 15:23:31 +0700

    Initialize database

---
+++ ddl.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Users (
+	user_id SERIAL PRIMARY KEY,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	password TEXT NOT NULL,
+	deposit_amount DECIMAL(10, 2) NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Cars (
+	car_id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	stock_availability INTEGER NOT NULL,
+	rental_costs DECIMAL(10, 2) NOT NULL,
+	category VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Rentals (
+	rental_id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES Users(user_id),
+	car_id INTEGER REFERENCES Cars(car_id),
+	duration INTEGER NOT NULL,
+	total_costs DECIMAL(10, 2) NOT NULL,
+	status VARCHAR(50) NOT NULL
+);
